833ba1f1e9b1926b0c83a8359ab0454ed5fc9811
Steven Davidovitz <steven.davidovitz@gmail.com>
2011-02-08 16:50:52 -0500

    toggle grabbing input while loading screen is displaying.

--- ui/ui_state.c
+++ ui/ui_state.c
@@ -14,8 +14,10 @@ void change_state(ui_state *next_state) {
 
 int prepare_state(void *data) {
   ui_state *state_to_prep = (ui_state *) data;
+  SDL_WM_GrabInput(SDL_GRAB_OFF);
   state_to_prep->preparing = true;
   state_to_prep->prepare();
   state_to_prep->preparing = false;
+  SDL_WM_GrabInput(SDL_GRAB_ON);
   return 0;
 }
