98910b0c12001ee5518a604e35dac9a01994bb14
Steven Davidovitz <steven.davidovitz@gmail.com>
2011-03-16 20:05:59 -0400

    flesh out Box element and add CenteredBox

--- interface/elements.rb
+++ interface/elements.rb
@@ -1,12 +1,20 @@
 class Box
-  def initialize(x, y, w, h, filled = false)
+  def initialize(x, y, w, h, filled = false, color = SDL::Color.new([0xff, 0, 0, 0xff]))
     @filled = filled
+    @color = color
     @rect = SDL::Rect.new([x, y, w, h])
-    @method = SDL::Gfx.method(filled ? :boxColor : :rectangleColor)
+    @method = SDL::Gfx.method(filled ? :boxRGBA : :rectangleRGBA)
   end
 
   def draw_to(surface)
     @method.call(surface, @rect.x, @rect.y,
-                 @rect.x + @rect.w, @rect.y + @rect.h, 0xff0000ff)
+                 @rect.x + @rect.w, @rect.y + @rect.h, 
+                 @color.r, @color.g, @color.b, 0xff)
+  end
+end
+
+class CenteredBox < Box
+  def initialize(x, y, w, h, filled = false, color = SDL::Color.new([0xff, 0, 0, 0xff]))
+    super(x - (w / 2), y - (h / 2), w, h, filled, color)
   end
 end
--- interface/menu.rb
+++ interface/menu.rb
@@ -4,8 +4,11 @@ class Menu < StateInterface
   def initialize
     super
 
-    @elements << Box.new(120, 120, 20, 20, true)
-    @elements << Box.new(420, 250, 20, 20)
+    @elements << Box.new(rand(WIDTH), rand(HEIGHT), 20, 20, true)
+    @elements << Box.new(rand(WIDTH), rand(HEIGHT), 20, 20)
+    @elements << Box.new(WIDTH / 2, HEIGHT / 2, 20, 20, true, SDL::Color.new([0, 0xff, 0, 0xff]))
+    @elements << Box.new(300, 40, 20, 20, false, SDL::Color.new([0, 0, 0xff, 0xff]))
+    @elements << CenteredBox.new(WIDTH / 2, HEIGHT / 2, WIDTH / 2, HEIGHT / 2)
   end
 
   def event(event)
